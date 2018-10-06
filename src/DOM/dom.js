767493ed4b118cdfe07290c4d191fd444633d398
koyata tezuka <koytez@gmail.com>
2018-10-05 19:40:16 -1000

    add board functionality

--- src/DOM/dom.js
+++ src/DOM/dom.js
@@ -1,13 +1,35 @@
 const dom = (() => {
 	return {
-		strings: {
-			board: '.board'
+		str: {
+			board: '.board',
+			turnBtn: '.btn-ship',
+			gameBtn: '.btn-game',
+			shipCont: '.ship-container'
 		},
-		nodeListForEach(list, callback) {
-			for (let i = 0; i < list.length; i++) {
-				callback(list[i], i);
+		f: {
+			// node forEach()
+			nodeListForEach(list, callback) {
+				for (let i = 0; i < list.length; i++) {
+					callback(list[i], i);
+				}
+			},
+			// mutli events in one listener
+			addListenerMulti(element, eventNames, listener) {
+				let events = eventNames.split(' ');
+				for (let i = 0; i < events.length; i++) {
+					element.addEventListener(events[i], listener, false);
+				}
+			},
+			// switch attribute
+			switchAttribute(targetList, otherPlayerList, attr = 'style', targetAttr, otherAttr) {
+				this.nodeListForEach(targetList , (cur, i) => {
+					cur.setAttribute(attr,targetAttr);
+				});
+				this.nodeListForEach(otherPlayerList, (cur, i) => {
+					cur.setAttribute(attr, otherAttr)
+				});
 			}
-		},
+		}
 	};
 })();
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,3 @@ import './DOM/events';
 import Ships from './modules/Ships';
 import './index.css';
 
-
-const destroyer = new Ships('destroyer', 5);
-
-const build = () => console.log('hello');
-
-build()
-console.log(destroyer)
