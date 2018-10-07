56f32c76548a9c171c79c603f11d5826e11c8e95
koyata tezuka <koytez@gmail.com>
2018-10-06 14:20:15 -1000

    reconfig to oop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import './DOM/events';
+import './modules/events';
 import Ships from './modules/Ships';
+import { DomClass, DomBtn } from './modules/DomClass';
+
 import './index.css';
 
+// init
+(function() {
+    const gameButton = new DomBtn(document.querySelector('.btn-game'));
+    
+
+    // toggle start - reset button
+	gameButton.toggleAttr(
+		'.ship-container',
+		'style',
+		'display: none',
+		'display: show'
+	);
+})();
